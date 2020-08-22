0d66fd11f2f044ff08cc2cd282f5b27d3631fb76
mathisfun271 <64607637+mathisfun271@users.noreply.github.com>
2020-08-21 22:07:19 -0500

    Update encryptOnly.py

--- encryptOnly.py
+++ encryptOnly.py
@@ -4,6 +4,29 @@ try:
 except:
     e=65537
 
+def modexp(b,e,m): #modular exponentation optimization (not my own)
+    X = b
+    E = e
+    Y = 1
+    while E > 0:
+        if E % 2 == 0:
+            X = (X * X) % m
+            E = E//2
+        else:
+            Y = (X * Y) % m
+            E = E - 1
+    return Y
+
+def txtToNum(s):#converts plaintext to raw binary string
+    s = s.lower()#Remove capitol letters
+    ret = ''
+    for el in s:
+        try:
+            ret+=bins[el]#adds binary digits for each character
+        except:1#does nothing if charater isn't in library
+    return ret
+
+maxenc = floor(log(n)/log(2))-1
 def encrypt(m):#Encrypts text. Public keys not needed, saved as variables
     if type(m) == str:#splits into sections less than the max single encryption length.
         rawbin = txtToNum(m) #raw binary
